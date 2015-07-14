d1386a53ca51a8a01457cb332e9e3b27cf24f565
MN <z.luckymn@gmail.com>
2015-07-14 12:23:39 +0800

    fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree. Use '--' to separate paths from revisions, like this: 'git <command> [<revision>...] -- [<file>...]'

---
+++ AutoUpdater/AutoUpdateHelper/CommonUnitity.cs
@@ -0,0 +1,102 @@
+﻿/*****************************************************************
+ * Copyright (C) Knights Warrior Corporation. All rights reserved.
+ * 
+ * Author:   圣殿骑士（Knights Warrior） 
+ * Email:    KnightsWarrior@msn.com
+ * Website:  http://www.cnblogs.com/KnightsWarrior/       http://knightswarrior.blog.51cto.com/
+ * Create Date:  5/8/2010 
+ * Usage:
+ *
+ * RevisionHistory
+ * Date         Author               Description
+ * 
+*****************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using AutoUpdater;
+
+namespace KnightsWarriorAutoupdater
+{
+    class CommonUnitity
+    {
+        public static string SystemBinUrl = AppDomain.CurrentDomain.BaseDirectory;
+
+        public static void RestartApplication()
+        {
+            Process.Start(Application.ExecutablePath);
+            Environment.Exit(0);
+        }
+
+        public static void CloseApplication()
+        {
+            OperProcess op = new OperProcess();
+            //启动进程
+            op.StartProcess();
+        }
+
+        #region 原有模式
+        //public static string GetFolderUrl(DownloadFileInfo file)
+        //{
+        //    string folderPathUrl = string.Empty;
+        //    int folderPathPoint = file.DownloadUrl.IndexOf("/", 15) + 1;
+        //    string filepathstring = file.DownloadUrl.Substring(folderPathPoint);
+        //    int folderPathPoint1 = filepathstring.IndexOf("/");
+        //    string filepathstring1 = filepathstring.Substring(folderPathPoint1 + 1);
+        //    if (filepathstring1.IndexOf("/") != -1)
+        //    {
+        //        string[] ExeGroup = filepathstring1.Split('/');
+        //        for (int i = 0; i < ExeGroup.Length - 1; i++)
+        //        {
+        //            folderPathUrl += "\\" + ExeGroup[i];
+        //        }
+        //        if (!Directory.Exists(SystemBinUrl + ConstFile.TEMPFOLDERNAME + folderPathUrl))
+        //        {
+        //            Directory.CreateDirectory(SystemBinUrl + ConstFile.TEMPFOLDERNAME + folderPathUrl);
+        //        }
+        //    }
+        //    return folderPathUrl;
+        //}
+        #endregion
+
+        public static string GetFolderUrl(DownloadFileInfo file,string tempFolderUrl="")
+        {
+            string folderPathUrl = string.Empty;
+            int folderPathPoint = file.DownloadUrl.IndexOf("/", 15) + 1;
+            string filepathstring = file.DownloadUrl.Substring(folderPathPoint);
+            //int folderPathPoint1 = filepathstring.IndexOf("/");
+            //string filepathstring1 = filepathstring.Substring(folderPathPoint1 + 1);
+            //if(filepathstring1.IndexOf("/") != -1)
+            if (filepathstring.IndexOf("/") != -1)
+            {
+                //string[] ExeGroup = filepathstring1.Split('/');
+                string[] ExeGroup = filepathstring.Split('/');
+                for (int i = 0; i < ExeGroup.Length - 1; i++)
+                {
+                    folderPathUrl += "\\" + ExeGroup[i];
+                }
+                if (string.IsNullOrEmpty(tempFolderUrl))
+                {
+                    tempFolderUrl = ConstFile.TEMPFOLDERNAME;
+                    if (!Directory.Exists(SystemBinUrl + tempFolderUrl + folderPathUrl))
+                    {
+                        Directory.CreateDirectory(SystemBinUrl + tempFolderUrl + folderPathUrl);
+                    }
+                }
+                else
+                {
+                    if (!Directory.Exists(SystemBinUrl + tempFolderUrl))
+                    {
+                        Directory.CreateDirectory(SystemBinUrl + tempFolderUrl);
+                    }
+                }
+               
+            }
+            return folderPathUrl;
+        }
+    }
+}
---
+++ AutoUpdater/AutoUpdateHelper/ConstFile.cs
@@ -0,0 +1,33 @@
+﻿/*****************************************************************
+ * Copyright (C) Knights Warrior Corporation. All rights reserved.
+ * 
+ * Author:   圣殿骑士（Knights Warrior） 
+ * Email:    KnightsWarrior@msn.com
+ * Website:  http://www.cnblogs.com/KnightsWarrior/       http://knightswarrior.blog.51cto.com/
+ * Create Date:  5/8/2010 
+ * Usage:
+ *
+ * RevisionHistory
+ * Date         Author               Description
+ * 
+*****************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KnightsWarriorAutoupdater
+{
+    public class ConstFile
+    {
+        public const string TEMPFOLDERNAME = "TempFolderBak";
+        public const string CONFIGFILEKEY = "config_";
+        public const string CONFIGFILE = "config";
+        public const string FILENAME = "AutoUpdater.config";
+        public const string ROOLBACKFILE = "WEBSiteUpdate.exe";
+        public const string MESSAGETITLE = "自动更新";
+        public const string CANCELORNOT = "更新正在进行是否取消";
+        public const string APPLYTHEUPDATE = "程序需要进行重启，请按ok充实";
+        public const string NOTNETWORK = "更新失败请重试";
+    }
+}
